80630a73a74098119aba487dd5abcd3c002913c2
Theodore Turocy <ted.turocy@gmail.com>
2019-06-27 14:07:31 +0100

    Extend tojson script to structure player data hierarchically.

--- averages/tojson.py
+++ averages/tojson.py
@@ -2,6 +2,7 @@ import sys
 import json
 from collections import OrderedDict
 
+import numpy as np
 import pandas as pd
 
 def dropnull(rec):
@@ -9,24 +10,70 @@ def dropnull(rec):
             for k, v, in rec.items()
             if isinstance(v, list) or not pd.isnull(v)}
 
+def xform_league(data):
+    return data.drop(['year', 'nameLeague'], axis='columns')
+
+def xform_person(data):
+    """Transform structure of person data columns.
+    """
+    if 'nameFirst' in data:
+        data['nameLast'] = data.apply(lambda x:
+                                      {'name':
+                                           dropnull({'last': x['nameLast'],
+                                                     'first': x['nameFirst']})},
+                                      axis=1)
+                                                
+        del data['nameFirst']
+    else:
+        data['nameLast'] = data.apply(lambda x:
+                                      {'name':
+                                           dropnull({'last': x['nameLast']})},
+                                      axis=1)
+    return data.rename(columns={'nameLast': 'person'})
+
+def xform_team_splits(data):
+    data['splits'] = data.apply(lambda x:
+                                [{"team": {"name": x[col]}}
+                                 for col in data.columns
+                                 if col.startswith('nameClub') and
+                                    not pd.isnull(x[col]) and x[col] != "all"],
+                                axis=1)
+    return data[[c for c in data.columns if not c.startswith('nameClub')]]
+
+def xform_totals(data):
+    data['totals'] = [dropnull(x)
+                      for x in data[[c for c in data.columns
+                                    if c not in ['row', 'person', 'splits']]]
+                                   .to_dict(orient='records')]
+    return data[['row', 'person', 'totals', 'splits']]
+
 def process_table(data):
+    data = data.assign(row=np.arange(len(data))+1) \
+               .pipe(xform_league) \
+               .pipe(xform_person) \
+               .pipe(xform_team_splits) \
+               .pipe(xform_totals)
     data = [dropnull(x) for x in data.to_dict(orient='records')]
     return data
 
 name_map = {'Batting':      'player_batting',
             'Pitching':     'player_pitching',
-            'Fielding':     'player_fielding',
-            'TeamBatting':  'team_batting',
-            'TeamPitching': 'team_pitching',
-            'TeamFielding': 'team_fielding',
-            'Standings':    'team_standings'}
+            'Fielding':     'player_fielding'}
+
 def main():
-    js = OrderedDict()
+    leagues = []
+    tables = OrderedDict()
     for (name, data) in pd.read_excel(sys.argv[1], sheet_name=None).items():
-        if name == 'entries':
-            continue
-        js[name_map[name]] = process_table(data)
-    
+        if name not in name_map: continue
+        leagues.append(data[['year', 'nameLeague']].drop_duplicates())
+        tables[name_map[name]] = process_table(data)
+
+    league = pd.concat(leagues, ignore_index=True) \
+               .drop_duplicates() \
+               .to_dict(orient='records')[0]
+    js = OrderedDict()
+    js['league'] = league
+    js['tables'] = tables
     js = json.dumps(js, indent=2)
     print(js)
 
